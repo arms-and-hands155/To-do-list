79549a9dc2e1c203aec351a748e300f7dfc27e66
arms-and-hands155 <armandnik16@Gmail.com>
2026-02-02 17:28:45 -0500

    Adding shake animation and working delete button for specific activities

--- index.js
+++ index.js
@@ -1,34 +1,52 @@
 let myLeads = [];
 const parent = document.getElementById("container");
-
 const inputEl = document.getElementById("input-el");
+
 
 function save(){
     myLeads.push(inputEl.value)
-    render(inputEl.value)
+    if(inputEl.value != ""){
+        render(inputEl.value)}
+    else{
+        document.body.classList.add('shake-effect');
+
+        setTimeout(function() {
+            document.body.classList.remove('shake-effect');
+        }, 500); 
+    }
     inputEl.value = ""
 }
 
 function reset(){
     parent.innerHTML = "";
 }
-
-
-
+//displays the activity entered
 function render(word) {
     const child = document.createElement('div');
+    const trash = document.createElement('img')
+    const edit = document.createElement('img')
+
+    trash.className = "trashClass";
+    edit.className = "editClass";
+
+    trash.src = "trash.png";
+    edit.src = "edit.png";
+
     child.className = 'childClass';
     child.textContent = word;
+
     parent.appendChild(child);
+    child.appendChild(trash);
+    child.appendChild(edit);
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    document.addEventListener("click", function (e) {
-        if (e.target.classList.contains("childClass")) {
-            e.target.style.background = "red";
-        }
-
-    });
+document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("childClass")) {
+        e.target.style.background = "red";
+    }
+    //removes cells that if trash is clicked
+    if (e.target.classList.contains("imgClass")){ 
+        const p_Node = e.target.parentNode; 
+        p_Node.remove();
+    }
 });
-
-
